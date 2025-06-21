32ae58ac4c5d6fc3e543981d535f97beea8808f4
Tasneem Natshah <t.natshah@gmail.com>
2025-06-21 14:39:26 +0300

    Append test script command to the package.json file #9

--- webship-js/v1/template.sh
+++ webship-js/v1/template.sh
@@ -175,8 +175,6 @@ PACKAGE_JSON_FILE="${local_project_path}/package.json"
 
 jq --arg name "$SCRIPT_NAME" --arg cmd "$SCRIPT_COMMAND" \
    '.scripts += {($name): $cmd}' "$PACKAGE_JSON_FILE" > temp.json && sudo mv temp.json "$PACKAGE_JSON_FILE"
-
-   echo "Script '$SCRIPT_NAME' added to $PACKAGE_JSON_FILE."
 fi
 
 ## Remove the old generate-reports.js file.
