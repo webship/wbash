9285eb826a8f8180863327634acd7a4d77edef85
Tasneem Natshah <t.natshah@gmail.com>
2025-06-21 10:24:51 +0300

    Change package.json by appending the webship-js package instead of replacing it #8

--- webship-js/v1/template.sh
+++ webship-js/v1/template.sh
@@ -142,10 +142,19 @@ if [[ -f "${local_project_path}/nightwatch.conf.js" ]]; then
   rm ${local_project_path}/nightwatch.conf.js;
 fi
 
-## Remove the old package.json file.
-if [[ -f "${local_project_path}/package.json" ]]; then
-  rm ${local_project_path}/package.json;
+## Append into package.json file.
+PACKAGE_NAME="webship-js"
+PACKAGE_VERSION="~1.0.0"
+
+PACKAGE_JSON_FILE="${local_project_path}/package.json"
+if ! command -v jq &> /dev/null
+then
+  sudo apt-get install jq
 fi
+jq --arg name "$PACKAGE_NAME" --arg version "$PACKAGE_VERSION" \
+   '.dependencies += {($name): $version}' "$PACKAGE_JSON_FILE" > temp.json && mv temp.json "$PACKAGE_JSON_FILE"
+
+echo "Dependency '$PACKAGE_NAME@$PACKAGE_VERSION' appended to $PACKAGE_JSON_FILE."
 
 ## Remove the old generate-reports.js file.
 if [[ -f "${local_project_path}/generate-reports.js" ]]; then
